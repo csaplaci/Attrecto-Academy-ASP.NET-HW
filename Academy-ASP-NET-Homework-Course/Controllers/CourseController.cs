c58cdb7b73bd25fc60bafd5d6361b78c443f96dd
László Csaplár <csapplaci@gmail.com>
2025-02-26 20:15:55 +0100

    feat(CourseController): CRUD (+new classes)

---
+++ Academy-ASP-NET-Homework-Course/Controllers/CourseController.cs
@@ -0,0 +1,69 @@
+﻿using Academy_ASP_NET_Homework_Course.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academy_ASP_NET_Homework_Course.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        public static List<Course>? Courses { get; set; }
+
+        //Create
+        [HttpPut]
+        public ActionResult Post([FromBody] Course data)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            Courses.Add(data);
+            return NoContent();
+        }
+
+        //Read
+        [HttpGet]
+        public ActionResult<Course> Get(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                    return Ok(course);
+            }
+            return NotFound();
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Course> Get()
+        {
+            return Courses;
+        }
+        
+        //Update
+        public ActionResult<Course> Put(int id, [FromBody] Course data)
+        {
+            foreach(var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    course.Name = data.Name;
+                    course.Description = data.Description;
+                    return NoContent();
+                }
+            }
+            return NotFound();
+        }
+
+        //Delete
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    Courses.Remove(course);
+                    return NoContent();
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Academy-ASP-NET-Homework-Course/Data/Course.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy_ASP_NET_Homework_Course.Data
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
