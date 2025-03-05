c816e78e96e0603b9557a5b258a7c32d53895f70
László Csaplár <csapplaci@gmail.com>
2025-03-05 20:56:31 +0100

    feat: users

---
+++ Academy-ASP-NET-Homework-Course/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using Academy_ASP_NET_Homework_Course.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academy_ASP_NET_Homework_Course.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public static List<User>? Users = new();
+        //Create
+        [HttpPost]
+        public ActionResult Post([FromBody] User data)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            Users.Add(data);
+            return NoContent();
+        }
+        //Read
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return Users;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            foreach (var user in Users)
+            {
+                if (user.Id == id)
+                    return Ok(user);
+            }
+            return NotFound();
+        }
+        //Update
+        [HttpPut("{id}")]
+        public ActionResult<User> Put(int id, [FromBody] User data)
+        {
+            foreach (var user in Users)
+            {
+                if (user.Id == id)
+                {
+                    user.FirstName = data.FirstName;
+                    user.LastName = data.LastName;
+                    user.Courses = data.Courses;
+                    return NoContent();
+                }
+            }
+            return NotFound();
+        }
+        //Delete
+        [HttpDelete]
+        public ActionResult<User> Delete(int id)
+        {
+            foreach (var user in Users)
+            {
+                if (user.Id == id)
+                {
+                    Users.Remove(user);
+                    return NoContent();
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Academy-ASP-NET-Homework-Course/Data/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy_ASP_NET_Homework_Course.Data
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? LastName { get; set; }
+        public ICollection<Course> Courses { get; set; } = [];
+    }
+}
